13370f5b608384ba852ffff9e41426174052a1ec
amithraj1823 <amithrajsn.ec22@rvce.edu.in>
2025-06-01 19:17:30 +0530

    Update top.v

--- src/top.v
+++ src/top.v
@@ -15,8 +15,7 @@ module top (
     input wire in1,
     input wire in0,
 
-    input wire [7:0] prev_data,
-    input wire invert_in, // for decode mode
+    input wire invert_in, // used during decode
 
     output wire out7,
     output wire out6,
@@ -27,53 +26,37 @@ module top (
     output wire out1,
     output wire out0,
 
-    output reg invert // valid in encode mode
+    output wire invert_out // valid only in encode mode
 );
 
-    wire [7:0] data_in;
-    wire [7:0] encoded_data;
+    wire [7:0] encoder_out, decoder_out;
     wire encoder_invert;
-    wire [7:0] decoded_data;
-    reg  [7:0] data_out;
 
-    // Combine individual input bits into a byte
-    assign data_in = {in7, in6, in5, in4, in3, in2, in1, in0};
-
-    // Split output byte into individual bits
-    assign out7 = data_out[7];
-    assign out6 = data_out[6];
-    assign out5 = data_out[5];
-    assign out4 = data_out[4];
-    assign out3 = data_out[3];
-    assign out2 = data_out[2];
-    assign out1 = data_out[1];
-    assign out0 = data_out[0];
-
+    // Internal encoder instance
     encoder enc (
-        .data_in(data_in),
-        .prev_data(prev_data),
-        .data_out(encoded_data),
+        .clk(clk),
+        .rst(rst),
+        .in7(in7), .in6(in6), .in5(in5), .in4(in4),
+        .in3(in3), .in2(in2), .in1(in1), .in0(in0),
+        .out7(encoder_out[7]), .out6(encoder_out[6]),
+        .out5(encoder_out[5]), .out4(encoder_out[4]),
+        .out3(encoder_out[3]), .out2(encoder_out[2]),
+        .out1(encoder_out[1]), .out0(encoder_out[0]),
         .invert(encoder_invert)
     );
 
+    // Internal decoder instance
     decoder dec (
-        .data_in(data_in),
+        .in7(in7), .in6(in6), .in5(in5), .in4(in4),
+        .in3(in3), .in2(in2), .in1(in1), .in0(in0),
         .invert(invert_in),
-        .data_out(decoded_data)
+        .out7(decoder_out[7]), .out6(decoder_out[6]),
+        .out5(decoder_out[5]), .out4(decoder_out[4]),
+        .out3(decoder_out[3]), .out2(decoder_out[2]),
+        .out1(decoder_out[1]), .out0(decoder_out[0])
     );
 
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
-            data_out <= 8'b0;
-            invert <= 0;
-        end else begin
-            if (select == 0) begin // encode
-                data_out <= encoded_data;
-                invert <= encoder_invert;
-            end else begin // decode
-                data_out <= decoded_data;
-                invert <= 0;
-            end
-        end
-    end
+    assign {out7, out6, out5, out4, out3, out2, out1, out0} = (select == 0) ? encoder_out : decoder_out;
+    assign invert_out = (select == 0) ? encoder_invert : 1'b0;
+
 endmodule
