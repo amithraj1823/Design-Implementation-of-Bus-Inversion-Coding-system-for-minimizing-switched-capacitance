b59c4021c5579755163335ee7272717ac841478a
amithraj1823 <amithrajsn.ec22@rvce.edu.in>
2025-06-01 19:18:08 +0530

    Update decoder.v

--- src/decoder.v
+++ src/decoder.v
@@ -1,7 +1,10 @@
 module decoder (
-    input wire [7:0] data_in,
+    input wire in7, in6, in5, in4, in3, in2, in1, in0,
     input wire invert,
-    output wire [7:0] data_out
+    output wire out7, out6, out5, out4, out3, out2, out1, out0
 );
-    assign data_out = invert ? ~data_in : data_in;
+    wire [7:0] data_in = {in7, in6, in5, in4, in3, in2, in1, in0};
+    wire [7:0] data_out = invert ? ~data_in : data_in;
+
+    assign {out7, out6, out5, out4, out3, out2, out1, out0} = data_out;
 endmodule
